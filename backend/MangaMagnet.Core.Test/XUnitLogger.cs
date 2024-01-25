951aeb1ff383047486fb7c885f0a7d0cabcb12e1
Yukine <devyukine@gmx.de>
2024-01-25 01:28:59 +0100

    test: add MangaMagnet.Core.Test

---
+++ backend/MangaMagnet.Core.Test/XUnitLogger.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.Extensions.Logging;
+using Xunit.Abstractions;
+
+namespace MangaMagnet.Core.Test;
+
+[ExcludeFromCodeCoverage]
+public class XunitLogger<T> : ILogger<T>, IDisposable
+{
+	private readonly ITestOutputHelper _output;
+
+	public XunitLogger(ITestOutputHelper output)
+		=> _output = output;
+
+	public void Dispose()
+	{
+		// Can be ignored
+	}
+
+	public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception,
+		Func<TState, Exception, string> formatter)
+		=> _output.WriteLine(state.ToString());
+
+	public bool IsEnabled(LogLevel logLevel)
+		=> true;
+
+	public IDisposable BeginScope<TState>(TState state)
+		=> this;
+}
