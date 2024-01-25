eae09d7553e9c861e188b0cd704994ce4c4eae75
Gerard Smit <gsmit1996@gmail.com>
2024-01-25 22:47:30 +0100

    Set base URL outside of generated client

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,5 +1,8 @@
+import { OpenAPI } from '@/services/openapi'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+
+OpenAPI.BASE = '';
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
