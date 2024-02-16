3ecbb1eab5633004f0b9d771fbe570937c5ad39a
Yukine <devyukine@gmx.de>
2024-02-16 17:47:45 +0100

    clean(MangaDexChapterResponse): remove not used JsonPropertyName attributes

--- backend/MangaMagnet.Core/Providers/MangaDex/Models/Api/Chapter/MangaDexChapterResponse.cs
+++ backend/MangaMagnet.Core/Providers/MangaDex/Models/Api/Chapter/MangaDexChapterResponse.cs
@@ -3,9 +3,9 @@
 namespace MangaMagnet.Core.Providers.MangaDex.Models.Api.Chapter;
 
 public record MangaDexChapterResponse<T>(
-	[property: JsonPropertyName("result")] string Result,
-	[property: JsonPropertyName("response")] string? Response,
-	[property: JsonPropertyName("data")] T Data,
+	string Result,
+	string? Response,
+	T Data,
 	[property: JsonPropertyName("limit")] int Limit,
 	[property: JsonPropertyName("offset")] int Offset,
 	[property: JsonPropertyName("total")] int Total
